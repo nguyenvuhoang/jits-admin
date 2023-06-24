9676e745757321c68ed1906b9339805e68c3f0a9
hoangnv <nguyenvuhoangz@gmail.com>
2023-06-24 12:31:37 +0700

    commit fix loiox

--- src/layouts/components/vertical/AppBarContent.tsx
+++ src/layouts/components/vertical/AppBarContent.tsx
@@ -11,9 +11,7 @@ import { Settings } from '@/@core/context/settingsContext'
 // ** Components
 import LanguageDropdown from '@/@core/layouts/components/shared-components/LanguageDropdown'
 import ModeToggler from '@/@core/layouts/components/shared-components/ModeToggler'
-import {
-  NotificationsType
-} from '@/@core/layouts/components/shared-components/NotificationDropdown'
+
 import { ShortcutsType } from '@/@core/layouts/components/shared-components/ShortcutsDropdown'
 import UserDropdown from '@/@core/layouts/components/shared-components/UserDropdown'
 
@@ -27,50 +25,7 @@ interface Props {
   saveSettings: (values: Settings) => void
 }
 
-const notifications: NotificationsType[] = [
-  {
-    meta: 'Today',
-    avatarAlt: 'Flora',
-    title: 'Congratulation Flora! 🎉',
-    avatarImg: '/images/avatars/4.png',
-    subtitle: 'Won the monthly best seller badge'
-  },
-  {
-    meta: 'Yesterday',
-    avatarColor: 'primary',
-    subtitle: '5 hours ago',
-    avatarText: 'Robert Austin',
-    title: 'New user registered.'
-  },
-  {
-    meta: '11 Aug',
-    avatarAlt: 'message',
-    title: 'New message received 👋🏻',
-    avatarImg: '/images/avatars/5.png',
-    subtitle: 'You have 10 unread messages'
-  },
-  {
-    meta: '25 May',
-    title: 'Paypal',
-    avatarAlt: 'paypal',
-    subtitle: 'Received Payment',
-    avatarImg: '/images/misc/paypal.png'
-  },
-  {
-    meta: '19 Mar',
-    avatarAlt: 'order',
-    title: 'Received Order 📦',
-    avatarImg: '/images/avatars/3.png',
-    subtitle: 'New order received from John'
-  },
-  {
-    meta: '27 Dec',
-    avatarAlt: 'chart',
-    subtitle: '25 hrs ago',
-    avatarImg: '/images/misc/chart.png',
-    title: 'Finance report has been generated'
-  }
-]
+
 
 const shortcuts: ShortcutsType[] = [
   {
